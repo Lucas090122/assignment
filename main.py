f93abb72c213e3f513e8fbe4c7fe088aa2c2114c
Qingyun Wang <lucas.qingyun@gmail.com>
2023-08-30 15:03:37 +0300

    just a test

--- main.py
+++
@@ -1,7 +0,0 @@
-pi = 3.14159265
-Format_pi = round(pi, 3)
-print("value of pi:", Format_pi)
-print()
-radius = float(input("Enter the radius: "))
-area_of_circle = Format_pi * radius ** 2
-print(round(area_of_circle, 3))
