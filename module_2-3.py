1b0526888150bf3c5b9a7c18b3618f5e4e4a6a5b
Qingyun Wang <lucas.qingyun@gmail.com>
2023-08-30 21:32:12 +0300

    complete

---
+++ module_2-3.py
@@ -0,0 +1,5 @@
+l = float(input("Please define the length of the rectangle:\n"))
+w = float(input("Please define the width of the rectangle:\n"))
+A = l * w
+P = 2 * (l + w)
+print(f"The area of the rectangle is {A}.\nThe perimeter of the rectangle is {P}")
