23602926f825beef93959741c8d6be6eff75f465
Qingyun Wang <lucas.qingyun@gmail.com>
2023-08-30 21:44:37 +0300

    complete

---
+++ module_2-4.py
@@ -0,0 +1,8 @@
+print("Please enter three integers")
+number_1 = int(input("First one: "))
+number_2 = int(input("Second one: "))
+number_3 = int(input("Third one: "))
+sum = number_1 + number_2 + number_3
+product = number_1 * number_2 * number_3
+average = sum / 3
+print(f"The sum of these integers is {sum}.\nThe product of these integers is {product}.\nThe average of these integers is {average}")
