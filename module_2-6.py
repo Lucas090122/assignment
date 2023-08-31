16a9382ab00d4d59f099e3e03dda4b5e71abecf8
Qingyun Wang <lucas.qingyun@gmail.com>
2023-08-31 21:41:17 +0300

    complete

---
+++ module_2-6.py
@@ -0,0 +1,6 @@
+import random
+random_list_1 = [random.randint(0,9) for _ in range(3)]
+print(f"The 3-digit code is: {random_list_1}")
+
+random_list_2 = [random.randint(1,6) for _ in range(4)]
+print(f"The 4-digit code is: {random_list_2}")
