9189d4dbd0fcb3a1af05433b0c538503b97bdd77
Qingyun Wang <lucas.qingyun@gmail.com>
2023-09-09 22:12:24 +0300

    changed on user doesn't enter number

--- module_4/module_4-3.py
+++ module_4/module_4-3.py
@@ -3,8 +3,14 @@ while True:
     num = input("Please enter a number(enter an empty string to quit):\n")
     if num == "":
         break
-    num = float(num)
-    list_1.append(num)
-i = max(list_1)
-j = min(list_1)
-print(f"The smallest number is {j}\nThe largest number is {i}")
+    elif num.isdigit():
+        num = float(num)
+        list_1.append(num)
+    else:
+        print("Wrong input format, please enter a number.")
+if list_1 == []:
+    print("You haven't enter any number.")
+else:
+    i = max(list_1)
+    j = min(list_1)
+    print(f"The smallest number is {j}\nThe largest number is {i}")
