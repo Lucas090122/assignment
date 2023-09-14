f71d522dba57213057faab3f2ee05947451a2282
Qingyun Wang <lucas.qingyun@gmail.com>
2023-09-14 23:59:27 +0300

    complete

--- module_5/module_5-4.py
+++ module_5/module_5-4.py
@@ -1,10 +1,10 @@
 print("Please provide 5 names of cities.")
 i = 0
 lst = []
-for i in range(0, 5):
+for i in range(5):
     city = input("Please enter a city name:")
     lst.append(city)
-    i+=1
+    i += 1
 print("The cities you chose are:")
 for city in lst:
     print(city)
