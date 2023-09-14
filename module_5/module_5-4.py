1d473a1356233d623de70b1d317e58034cad3d0d
Qingyun Wang <lucas.qingyun@gmail.com>
2023-09-14 23:08:01 +0300

    complete

---
+++ module_5/module_5-4.py
@@ -0,0 +1,10 @@
+print("Please provide 5 names of cities.")
+i = 0
+lst = []
+for i in range(0, 5):
+    city = input("Please enter a city name:")
+    lst.append(city)
+    i+=1
+print("The cities you chose are:")
+for city in lst:
+    print(city)
